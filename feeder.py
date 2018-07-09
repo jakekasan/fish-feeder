d7a2d28f3e2483659a6a4eedc953533fbd758d6b
jakekasan <jake.kasan@gmail.com>
2018-07-09 08:59:52 +0200

    added stop and start for feeder

--- feeder.py
+++ feeder.py
@@ -1,9 +1,12 @@
 import datetime as dt
+import RPi.GPIO as GPIO
+
 
 class Feeder:
     def __init__(self):
         self.schedule = {}
-        self.feederGPIO = None
+        self.feeder_GPIO = None
+        self.feeder_running = False
 
     def add_value(self,amount,given_time=None):
         if time not None:
@@ -15,12 +18,21 @@ class Feeder:
         self.schedule = {}
 
     def run_feeder(self,value):
-        self.feederGPIO = True
+        self.feeder_GPIO = True
         time.sleep(value)
-        self.feederGPIO = False
+        self.feeder_GPIO = False
 
-    def 
-        
+    def start_feeder(self):
+        if self.feeder_running:
+            return
+        print("starting feeder")
+        self.feeder_running = True
+
+    def stop_feeder(self):
+        if not self.feeder_running:
+            return
+        print("stopping feeder")
+        self.feeder_running = False
 
     def update(self):
         time_now = dt.datetime.now()
