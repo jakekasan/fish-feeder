45faf7ed24c9e93a566727b7befe12c264c7fae6
jakekasan <jake.kasan@gmail.com>
2018-07-09 09:36:32 +0200

    feeder now runs perpetually with a default schedule

--- feeder.py
+++ feeder.py
@@ -1,46 +1,63 @@
+import time
 import datetime as dt
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
 
 
 class Feeder:
-    def __init__(self):
-        self.schedule = {}
+    def __init__(self,schedule=None):
+        if not schedule:
+            self.schedule = {}
+        else:
+            self.schedule = schedule
         self.feeder_GPIO = None
         self.feeder_running = False
+        print("Feeder created")
+        print("Schedule:")
+        print("Start\tEnd")
+        for key,value in self.schedule.iteritems():
+            print("{}\t{}".format(key,value))
 
-    def add_value(self,amount,given_time=None):
-        if time not None:
-            self.schedule[given_time] = amount
+    def add_value(self,given_time=None,end_time=None):
+        print("Adding value to schedule")
+        if not given_time == None:
+            self.schedule[given_time] = end_time
         else:
-            self.schedule[dt.datetime.now()]
+            now = dt.datetime.now()
+            then = dt.datetime(now.year,now.month,now.day,now.hour,now.minute,now.second+5)
+            self.schedule[now] = then
 
     def clear(self):
         self.schedule = {}
 
     def run_feeder(self,value):
-        self.feeder_GPIO = True
-        time.sleep(value)
-        self.feeder_GPIO = False
+        print("Starting feeder")
+        self.start_feeder
+        while True:
+            now = dt.datetime.now()
+            if (now.hour == value.hour) and (now.minute == value.minute) and (now.second == value.second):
+                print("End time reached")
+                break
+            time.sleep(1)
+        print("End of feeder function")
 
     def start_feeder(self):
         if self.feeder_running:
             return
-        print("starting feeder")
         self.feeder_running = True
+        print("started feeder")
+        time.sleep(1)
 
     def stop_feeder(self):
         if not self.feeder_running:
             return
-        print("stopping feeder")
         self.feeder_running = False
+        print("stopped feeder")
+        time.sleep(1)
 
     def update(self):
         time_now = dt.datetime.now()
-        for time_then in self.schedule.keys:
-            if time_then.hour == time_now.hour:
-                self.runFeeder(self.schedule[time_then])
+        for time_then in self.schedule.keys():
+            if (time_then.hour == time_now.hour) and (time_then.minute == time_now.minute) and (time_then.second == time_now.second):
+                self.run_feeder(self.schedule[time_then])
 
 
-    
-
-    
--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 #!/bin/usr/env python3
 
+from feeder import Feeder
+import datetime as dt
+
 def main():
-    print("hello!")
+    feeder = Feeder()
+
+    while True:
+        feeder.update()
 
 if "__main__" == __name__:
     main()
 
+    
+now = dt.datetime.now()
+default_schedule = {
+    dt.datetime(2015,12,1,now.hour,now.minute+1,1):dt.datetime(2015,12,1,now.hour,now.minute,5),
+    dt.datetime(2015,12,1,now.hour,now.minute+1,7):dt.datetime(2015,12,1,now.hour,now.minute+1,10),
+    dt.datetime(2015,12,1,now.hour,now.minute+1,12):dt.datetime(2015,12,1,now.hour,now.minute+1,15),
+    dt.datetime(2015,12,1,now.hour,now.minute+1,20):dt.datetime(2015,12,1,now.hour,now.minute+1,21)
+}
