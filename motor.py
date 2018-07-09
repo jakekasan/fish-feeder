b89a0645e00656fe02cd9d464555fa94fe79bdb1
jakekasan <jake.kasan@gmail.com>
2018-07-09 22:48:50 +0200

    added motor class to deal with GPIO

---
+++ motor.py
@@ -0,0 +1,12 @@
+class Motor:
+    def __init__(self,gpio_pin):
+        self.gpio_pin = gpio_pin
+        # GPIO pin stuff
+
+    def start_motor(self):
+        # start motor
+        return
+
+    def stop_motor(self):
+        # end and cleanup motor
+        return
