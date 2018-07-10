a2ff43ce4d8b7f1bb2e38ac2a767f98f786306b2
jakekasan <jake.kasan@gmail.com>
2018-07-10 07:35:02 +0200

    added l293d code

--- motor.py
+++ motor.py
@@ -1,6 +1,14 @@
+# import l293d.driver as l293d
+
+# GPIO Pins:
+# pin 22
+# pin 18
+# pin 16
+
 class Motor:
     def __init__(self,gpio_pin):
         self.gpio_pin = gpio_pin
+        # self.motor = l293d.motor(22,18,16)
         # GPIO pin stuff
 
     def start_motor(self):
